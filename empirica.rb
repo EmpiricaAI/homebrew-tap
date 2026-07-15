80782ba8bec5141d230949bacbd17c7bbecbd04a
Empirica System <david@getempirica.com>
2026-07-15 19:44:09 +0200

    Update empirica to 1.12.23

--- empirica.rb
+++ empirica.rb
@@ -13,8 +13,8 @@ class Empirica < Formula
 
   desc "Epistemic self-assessment framework for AI agents"
   homepage "https://github.com/EmpiricaAI/empirica"
-  url "https://files.pythonhosted.org/packages/source/e/empirica/empirica-1.12.22.tar.gz"
-  sha256 "ca9b970552b40af9ea6ff38b4e250d06bf1fba4dcadf96908c23f25a7b3150ff"
+  url "https://files.pythonhosted.org/packages/source/e/empirica/empirica-1.12.23.tar.gz"
+  sha256 "775c87dc5c2bf5ed1c54744ede45076e6391071c6fdd18da845650721cd1d61d"
   license "MIT"
 
   depends_on "python@3.11"
@@ -49,7 +49,7 @@ class Empirica < Formula
 
   test do
     # Test that the CLI works
-    assert_match "1.12.22", shell_output("#{bin}/empirica --version")
+    assert_match "1.12.23", shell_output("#{bin}/empirica --version")
 
     # Test that key commands exist
     system "#{bin}/empirica", "session-create", "--help"
